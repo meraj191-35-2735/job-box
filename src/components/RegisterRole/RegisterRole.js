ad654c2d6e47799214f5d6f98123e5933718fdc6
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2023-01-28 22:40:21 +0600

    something done

--- src/components/RegisterRole/RegisterRole.js
+++ src/components/RegisterRole/RegisterRole.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import candidate from "../../assests/images/registerRole/candidate.jpg";
+import employer from "../../assests/images/registerRole/employer.jpg";
+import { useNavigate } from "react-router-dom";
 
 const RegisterRole = () => {
-    return (
-        <div>
-            
+  const navigate = useNavigate();
+  const handleRegister = (role) => {
+    navigate(`/register/${role}`);
+  };
+
+  return (
+    <div className="mx-20 border rounded mt-2">
+      <div className="flex justify-center items-center h-10">
+        <p className="font-serif font-semibold italic">Continue as ...</p>
+      </div>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-2 m-3">
+        <div
+          onClick={() => handleRegister("candidate")}
+          className="border-2 flex flex-col justify-center items-center hover:border-2 hover:border-accent hover:shadow-2xl hover:rounded-lg"
+        >
+          <img className="w-96 h-80" src={candidate} alt="" />
+          <div>
+            <p className="font-serif font-bold p-2 m-2">Candidate</p>
+          </div>
         </div>
-    );
+        <div
+          onClick={() => handleRegister("employer")}
+          className="border-2 flex flex-col justify-center items-center hover:border-2 hover:border-accent hover:shadow-2xl hover:rounded-lg"
+        >
+          <img className="w-96 h-80 " src={employer} alt="" />
+          <div>
+            <p className="font-serif font-bold p-2 m-2">Employer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterRole;
---
+++ src/components/RegisterRole/RegisterUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { Link, useParams } from "react-router-dom";
+
+const RegisterUser = () => {
+  const { role } = useParams();
+  return (
+    <div className="lg:mx-40 mt-5 border-2">
+      <Link className="flex items-center text-sm m-2" to="/registerRole">
+        <FiArrowLeftCircle></FiArrowLeftCircle>
+        <p className="font-serif ml-0.5">Back</p>
+      </Link>
+      <div className="w-3/4 mx-auto">
+        <h2 className="font-bold font-mono">
+          <span className="uppercase">{role} </span>Registration
+        </h2>
+        <form></form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterUser;
